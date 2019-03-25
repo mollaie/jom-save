dd4d8630b4d1c93610b9a693cdd2222044ac4b34
mollaie <47516605+mollaie@users.noreply.github.com>
2019-03-25 20:15:11 +0800

    remove android platform for bundle fixing

--- www/cordova_plugins.js
+++ www/cordova_plugins.js
@@ -268,6 +268,22 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-file-transfer.FileTransferError",
+    "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
+    "pluginId": "cordova-plugin-file-transfer",
+    "clobbers": [
+      "window.FileTransferError"
+    ]
+  },
+  {
+    "id": "cordova-plugin-file-transfer.FileTransfer",
+    "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
+    "pluginId": "cordova-plugin-file-transfer",
+    "clobbers": [
+      "window.FileTransfer"
+    ]
+  },
+  {
     "id": "cordova-plugin-firebase.FirebasePlugin",
     "file": "plugins/cordova-plugin-firebase/www/firebase.js",
     "pluginId": "cordova-plugin-firebase",
@@ -309,6 +325,14 @@ module.exports = [
     ]
   },
   {
+    "id": "cordova-plugin-ionic-webview.IonicWebView",
+    "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
+    "pluginId": "cordova-plugin-ionic-webview",
+    "clobbers": [
+      "Ionic.WebView"
+    ]
+  },
+  {
     "id": "cordova-plugin-secure-storage.SecureStorage",
     "file": "plugins/cordova-plugin-secure-storage/www/securestorage.js",
     "pluginId": "cordova-plugin-secure-storage",
@@ -346,6 +370,20 @@ module.exports = [
     "pluginId": "cordova-plugin-uniquedeviceid",
     "merges": [
       "window.plugins.uniqueDeviceID"
+    ]
+  },
+  {
+    "id": "es6-promise-plugin.Promise",
+    "file": "plugins/es6-promise-plugin/www/promise.js",
+    "pluginId": "es6-promise-plugin",
+    "runs": true
+  },
+  {
+    "id": "cordova-plugin-x-socialsharing.SocialSharing",
+    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
+    "pluginId": "cordova-plugin-x-socialsharing",
+    "clobbers": [
+      "window.plugins.socialsharing"
     ]
   },
   {
@@ -443,44 +481,6 @@ module.exports = [
     "merges": [
       "cordova.plugins.diagnostic.external_storage"
     ]
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransferError",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransferError.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransferError"
-    ]
-  },
-  {
-    "id": "cordova-plugin-file-transfer.FileTransfer",
-    "file": "plugins/cordova-plugin-file-transfer/www/FileTransfer.js",
-    "pluginId": "cordova-plugin-file-transfer",
-    "clobbers": [
-      "window.FileTransfer"
-    ]
-  },
-  {
-    "id": "es6-promise-plugin.Promise",
-    "file": "plugins/es6-promise-plugin/www/promise.js",
-    "pluginId": "es6-promise-plugin",
-    "runs": true
-  },
-  {
-    "id": "cordova-plugin-x-socialsharing.SocialSharing",
-    "file": "plugins/cordova-plugin-x-socialsharing/www/SocialSharing.js",
-    "pluginId": "cordova-plugin-x-socialsharing",
-    "clobbers": [
-      "window.plugins.socialsharing"
-    ]
-  },
-  {
-    "id": "cordova-plugin-ionic-webview.IonicWebView",
-    "file": "plugins/cordova-plugin-ionic-webview/src/www/util.js",
-    "pluginId": "cordova-plugin-ionic-webview",
-    "clobbers": [
-      "Ionic.WebView"
-    ]
   }
 ];
 module.exports.metadata = 
@@ -492,22 +492,22 @@ module.exports.metadata =
   "cordova-plugin-camera": "4.0.3",
   "cordova-plugin-cszbar": "1.3.2",
   "cordova-plugin-device": "2.0.2",
+  "cordova-plugin-file-transfer": "1.7.1",
   "cordova-plugin-firebase": "2.0.5",
   "cordova-plugin-globalization": "1.11.0",
   "cordova-plugin-inappbrowser": "3.0.0",
   "cordova-plugin-ionic-keyboard": "2.1.3",
+  "cordova-plugin-ionic-webview": "4.0.0",
   "cordova-plugin-secure-storage": "3.0.1",
   "cordova-plugin-splashscreen": "5.0.2",
   "cordova-plugin-statusbar": "2.4.2",
   "cordova-plugin-themeablebrowser": "0.2.17",
   "cordova-plugin-uniquedeviceid": "1.3.2",
   "cordova-plugin-whitelist": "1.3.3",
-  "cordova-sqlite-storage": "3.1.0",
-  "cordova.plugins.diagnostic": "4.0.11",
-  "cordova-plugin-file-transfer": "1.7.1",
   "es6-promise-plugin": "4.2.2",
   "cordova-plugin-x-socialsharing": "5.4.4",
-  "cordova-plugin-ionic-webview": "4.0.0"
+  "cordova-sqlite-storage": "3.1.0",
+  "cordova.plugins.diagnostic": "4.0.11"
 };
 // BOTTOM OF METADATA
 });
